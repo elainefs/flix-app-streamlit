3f97364cd0702fe16b4d7a41a42a90043db108ce
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-04-08 15:31:30 -0300

    add home

---
+++ home/page.py
@@ -0,0 +1,34 @@
+import plotly.express as px
+import streamlit as st
+
+from movies.service import MovieService
+
+
+def show_home():
+    movie_service = MovieService()
+    movie_stats = movie_service.get_movie_stats()
+
+    st.title("Mostrar Estatísticas")
+
+    if len(movie_stats["movies_by_genre"]) > 0:
+        st.subheader("Filmes por Gênero")
+        fig = px.pie(
+            movie_stats["movies_by_genre"],
+            values="count",
+            names="genre__name",
+            title="Filmes por Gênero",
+        )
+        st.plotly_chart(fig)
+
+    st.subheader("Total de Filmes Cadastrados:")
+    st.write(movie_stats["total_movies"])
+
+    st.subheader("Quantidade de Filmes por Gênero:")
+    for genre in movie_stats["movies_by_genre"]:
+        st.write(f"{genre['genre__name']}: {genre['count']}")
+
+    st.subheader("Total de Avaliações Cadastradas:")
+    st.write(movie_stats["total_reviews"])
+
+    st.subheader("Média Geral de Estrelas nas Avaliações:")
+    st.write(movie_stats["average_stars"])
